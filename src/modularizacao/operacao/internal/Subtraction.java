c1f9f1706c766fa1bd13296e77908d8d3a93105b
Jhonatan Santos <jhony.jpn@gmail.com>
2021-03-09 12:50:49 -0300

    Subtraction

--- src/modularizacao/operacao/internal/Subtraction.java
+++ src/modularizacao/operacao/internal/Subtraction.java
@@ -1,2 +1,10 @@
-package modularizacao.operacao.internal;public class Subtraction {
+package modularizacao.operacao.internal;
+
+import modularizacao.operacao.Operacao;
+
+public class Subtraction implements Operacao {
+    @Override
+    public int executarOperacao(int x, int y) {
+        return x-y;
+    }
 }
