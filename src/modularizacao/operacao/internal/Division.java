91d7aa67f8103c872afb2f35635b9d0abc929870
Jhonatan Santos <jhony.jpn@gmail.com>
2021-03-09 12:49:54 -0300

    Division

--- src/modularizacao/operacao/internal/Division.java
+++ src/modularizacao/operacao/internal/Division.java
@@ -1,2 +1,10 @@
-package modularizacao.operacao.internal;public class Division {
+package modularizacao.operacao.internal;
+
+import modularizacao.operacao.Operacao;
+
+public class Division implements Operacao {
+    @Override
+    public int executarOperacao(int x, int y) {
+        return x/y;
+    }
 }
