91a5331538152dfa58d062e2c5db6f2b97c0f660
Jhonatan Santos <jhony.jpn@gmail.com>
2021-03-09 12:48:52 -0300

    Calculadora

--- src/modularizacao/operacao/Calculadora.java
+++ src/modularizacao/operacao/Calculadora.java
@@ -1,4 +1,34 @@
 package modularizacao.operacao;
+import modularizacao.operacao.internal.Multiplication;
+import modularizacao.operacao.internal.Division;
+import modularizacao.operacao.internal.Subtraction;
+import modularizacao.operacao.internal.Sum;
+
 
 public class Calculadora {
+    private Division div;
+    private Multiplication multi;
+    private Subtraction sub;
+    private Sum sum;
+
+    public Calculadora() {
+        div = new Division();
+        multi = new Multiplication();
+        sub = new Subtraction();
+        sum = new Sum();
+    }
+
+    public int somar(int x,int y){
+        return sum.executarOperacao(x,y);
+    }
+    public int dividir(int x, int y){
+        return div.executarOperacao(x,y);
+    }
+    public int multiplicar(int x,int y){
+        return multi.executarOperacao(x,y);
+    }
+    public int subtrair(int x, int y){
+        return sub.executarOperacao(x,y);
+    }
+
 }
